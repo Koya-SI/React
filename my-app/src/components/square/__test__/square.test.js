a8c75acbb8b530275a46c49b730449f9ac519f12
Koya-SI <koya.sone@sharing-innovations.co.jp>
2020-12-14 18:16:40 +0900

    WIP: React tutorial

--- my-app/src/components/square/__test__/square.test.js
+++ my-app/src/components/square/__test__/square.test.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Square from './../square';
-import renderer from 'react-test-renderer'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Square from "./../square";
+import renderer from "react-test-renderer";
 
 it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Square></Square>, div)
-})
+  const div = document.createElement("div");
+  ReactDOM.render(<Square></Square>, div);
+});
 
 it("matches snapshot", () => {
-    const tree = renderer.create(<Square></Square>).toJSON();
-    expect(tree).toMatchSnapshot();
-})
+  const tree = renderer.create(<Square></Square>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
