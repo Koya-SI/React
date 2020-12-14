fe73d75d977c1d8b231d00e6d9ab23d230c28122
Koya-SI <koya.sone@sharing-innovations.co.jp>
2020-12-14 18:20:27 +0900

    WIP: React tutorial
    コミット内容の詳細
    interface.tsのファイル名変更とPrettierを使ってコードを整えました。

--- my-app/src/components/square/square.tsx
+++ my-app/src/components/square/square.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { ISquare } from '../../interface';
+import React from "react";
+import { ISquare } from "../../ISquare";
 
 interface SquareProps {
   value: ISquare;
   onClick: () => void;
 }
 
-export default function Square(props:SquareProps) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
+export default function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './components/style/index.css';
-import Game from './components/game/game';
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+import React from "react";
+import ReactDOM from "react-dom";
+import "./components/style/index.css";
+import Game from "./components/game/game";
+
+ReactDOM.render(<Game />, document.getElementById("root"));
