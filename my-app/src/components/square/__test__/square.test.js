6437cd21f2cef872fe2cdd25b46cbfa4f8954064
Koya-SI <koya.sone@sharing-innovations.co.jp>
2020-12-16 17:24:39 +0900

    WIP: React tutorial
    onClickのイベント処理のテストコードを作成中

--- my-app/src/components/square/__test__/square.test.js
+++ my-app/src/components/square/__test__/square.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Square from './../square';
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
+import shallow  from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+Enzyme.configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
@@ -13,3 +17,12 @@ it("matches snapshot", () => {
     const tree = renderer.create(<Square></Square>).toJSON();
     expect(tree).toMatchSnapshot();
 })
+
+describe('clickイベントのテスト', ()=>{
+  test('Squareコンポーネントにイベントを渡す', ()=>{
+    const testMock = jest.fn();
+    const subject = shallow(<Square event={testMock} />);
+    subject.find('button').simulate('click');
+    expect(testMock).toHaveBeenCalled();
+  });
+});
